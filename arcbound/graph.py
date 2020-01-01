a69e093060aa3b62cec2c953ef5e0fd51e83d87c
Jason Hwang <jhwang.astro@gmail.com>
2020-01-01 11:10:18 -0800

    Compartmentalize arcbound properties
    To avoid cluttering the attribute namespace of the object decorated with arcbound.graph, an arcbound_graph property was added containing the methods, properties, and nodes properties as well as the get_node function.

--- arcbound/graph.py
+++ arcbound/graph.py
@@ -4,13 +4,113 @@ decorated with arcbound.arc.
 
 import functools
 import inspect
-from typing import Callable, Dict, Type, TypeVar
+from typing import Callable, Dict, List, Tuple, Type, TypeVar
 
 from .arc import arc
 
 T = TypeVar("T")
+ArcboundGraph = TypeVar("ArcboundGraph", bound="ArcboundGraph")
 
-def graph(cls: T) -> T:
+def create_arcbound_graph(cls: T) -> ArcboundGraph:
+    """ Returns an ArcboundGraph object created using an input class.
+
+    The ArcboundGraph object contains properties summarizing the attributes of
+    the class decorated with arcbound.arc and a method for returning underlying
+    functions generating the properties and methods.
+
+    Args:
+        cls: Class to graph methods and properties decorated with arcbound.arc.
+    """
+    class ArcboundGraph(object):
+        """
+        Attributes:
+            class_attrs: List of attribute names belonging to the input class.
+            properties: Dictionary mapping methods decorated with both
+                property and arcbound.arc to the property name.
+            methods: Dictionary mapping methods decorated with arcbound.arc.
+            nodes: Dictionary combining the properties and methods dictionaries.
+            arcs: Dictionary mapping dependencies of methods decorated with
+                arcbound.arc.
+            
+        Functions:
+            get_node: Returns as a curried function the decorated method.
+        """
+        @property
+        @functools.lru_cache()
+        def class_attrs(self) -> List[str]:
+            """ Returns a list of attribute names belonging to the input class.
+            """
+            return dir(cls)
+
+        @property
+        @arc(functions_list="class_attrs")
+        def properties(self, functions_list: List[str]) -> Dict[str, Callable]:
+            """ Returns a dictionary mapping property names to the method
+            defining the property for methods decorated with arcbound.arc.
+            """
+            return {
+                function_name: method.fget
+                for function_name in functions_list 
+                for method in [getattr(cls, function_name)]
+                if isinstance(method, property)
+                if hasattr(method.fget, "arc")
+            }
+        
+        @property
+        @arc(functions_list="class_attrs")
+        def methods(self, functions_list: List[str]) -> Dict[str, Callable]:
+            """ Returns a dictionary mapping method names and methods for
+            methods decorated with arcbound.arc.
+            """
+            return {
+                function_name: method
+                for function_name in functions_list 
+                for method in [getattr(cls, function_name)]
+                if hasattr(method, "arc")
+            }
+        
+        @property
+        @arc(d1="properties", d2="methods")
+        def nodes(
+            self,
+            d1: Dict[str, Callable],
+            d2: Dict[str, Callable]
+        ) -> Dict[str, Callable]:
+            """ Combines the properties and methods into a single dictionary.
+            """
+            return {**d1, **d2}
+
+        @property
+        @arc(nodes="nodes")
+        def arcs(self, nodes: Dict[str, Callable]) -> Dict[str, Tuple[str]]:
+            """ Returns a dictionary mapping dependencies by method/property.
+            """
+            return {
+                name: {dep for dep in method.arc}
+                for name, method in nodes.items()
+            }
+        
+        @arc(nodes="nodes")
+        def get_node(self, k: str, nodes: Dict[str, Callable]) -> Callable:
+            """ Returns the function defining the method or property.
+            """
+            def default_f(cls, *args, **kwargs) -> None:
+                """ Dummy function returning None and indicating if the
+                requested method is defined and/or decorated.
+                """
+                if k in dir(cls):
+                    print("This method is not decorated.")
+                else:
+                    print("This method does not exist.")
+
+                return None
+
+            return nodes.get(k, default_f)
+
+    return ArcboundGraph
+
+
+def graph(cls: T) -> Callable[[T], T]:
     """ Returns a class with properties and functions leveraging methods
     decorated with the arcbound.arc function.
 
@@ -34,80 +134,58 @@ def graph(cls: T) -> T:
             @ab.arc(x="branch", y="branch")
             def leaf(self, x: int, y: int) -> int:
                 return x * y
+
+            def twig(self, x: int, y: int) -> int:
+                return x * y
+
+        Test = test(5)
+        Test.arcbound_graph.methods
+        # {'leaf': <function __main__.test.leaf(self, x:int, y:int) -> int>}
+        Test.arcbound_graph.properties
+        # {'branch': <function __main__.test.branch(self, x:int) -> int>}
+        Test.arcbound_nodes
+        # {'branch': <function __main__.test.branch(self, x:int) -> int>,
+        #  'leaf': <function __main__.test.leaf(self, x:int, y:int) -> int>}
+        Test.get_arcbound_node("branch")(10)
+        # 100
+        Test.get_arcbound_node("leaf")(2, 3)
+        # 6
+        Test.get_arcbound_node("twig")(2, 3)
+        # This method is not decorated.
+        Test.get_arcbound_node("nest")(10)
+        # This method does not exist.
     """
-    class wrapper_class(cls) -> T:
+    class wrapper_class(cls):
         """ Returns a class with properties and functions leveraging methods
         decorated with the arcbound.arc function.
 
         Attributes:
-            arcbound_properties:
-            arcbound_methods:
-            arcbound_nodes:
+            arcbound_graph: Returns an ArcboundGraph summarizing the input
+                class's decorated methods.
             
         Functions:
-            get_arcbound_node:
+            get_arcbound_node: Returns a curried function with the class's self
+                provided as the class instance.
         """
         @property
-        def arcbound_properties(self) -> Dict[str, Callable]:
-            """ Returns a dictionary mapping property names to the method
-            defining the property for methods decorated with arcbound.arc.
+        def arcbound_graph(self) -> ArcboundGraph:
+            """ Returns an ArcboundGraph object used to summarize and interact
+            with attributes decorated with arcbound.arc.
             """
-            return {
-                function_name: method.fget
-                for function_name in dir(cls)
-                for method in [getattr(cls, function_name)]
-                if isinstance(method, property)
-                if hasattr(method.fget, "arc")
-            }
+            return create_arcbound_graph(cls)()
         
-        @property
-        def arcbound_methods(self) -> Dict[str, Callable]:
-            """ Returns a dictionary mapping method names and methods for
-            methods decorated with arcbound.arc.
-            """
-            return {
-                function_name: method
-                for function_name in dir(cls)
-                for method in [getattr(cls, function_name)]
-                if (hasattr(method, "arc") & inspect.ismethod(method))
-            }
-        
-        @property
-        @arc(d1="arcbound_properties", d2="arcbound_methods")
-        def arcbound_nodes(
-            self,
-            d1: Dict[str, Callable],
-            d2: Dict[str, Callable]
-        ) -> Dict[str, Callable]:
-            """ Combines the arcbound_properties and arcbound_methods into a
-            single dictionary.
-            """
-            return {
-                **self.arcbound_properties,
-                **self.arcbound_methods
-            }
-        
-        @arc(nodes="arcbound_nodes")
+        @arc(arcbound_graph="arcbound_graph")
         def get_arcbound_node(
             self,
             k: str,
-            nodes: Dict[str, Callable]
+            arcbound_graph: ArcboundGraph 
         ) -> Callable:
             """ Returns the function defining the method or property with the
             instance variable already assigned.
             """
-            def default_f(self, *args, **kwargs) -> None:
-                """ Dummy function returning None and indicating if the
-                requested method is defined and/or decorated.
-                """
-                if k in dir(cls):
-                    print("This method is not decorated.")
-                else:
-                    print("This method does not exist.")
+            f = arcbound_graph.get_node(k)
 
-                return None
+            return functools.partial(f, self)
 
-            return functools.partial(nodes.get(k, default_f), self)
-        
     return wrapper_class
 
