60c5ea3d7be066719ed21e97b0a7aee68db02a0e
Jason Hwang <jhwang.astro@gmail.com>
2020-01-25 08:51:07 -0800

    Update how class inherits docstring
    Changing how the docstring is set to the parent class's docstring allows for the help menu to find the parent class's docstring.

--- arcbound/requires.py
+++ arcbound/requires.py
@@ -13,8 +13,9 @@ def requires(
     package: str,
     message: str = None,
     warn_only: bool = False,
-) -> Callable[..., ClassType]:
-    """ Returns a function checking if the requested package is installed.
+) -> Callable[[ClassType], ClassType]:
+    """ Returns the input class with an added check that the input package
+    is installed at instantiation.
 
     Args:
         package: Name of package to check for.
@@ -22,23 +23,39 @@ def requires(
         warn_only: Flag determining if import errors should be downgraded to
             a warning.
     """
-    def check_package(cls: ClassType) -> ClassType:
-        """ Returns a function checking if the requested package is installed.
-        Raises an import error if the requested package is not installed,
-        otherwise returns the class.
+    def wrapper_factory(cls: ClassType) -> ClassType:
+        """ Returns the input class with an added check that a specified package
+        is installed at instantiation.
+
+        Args:
+            cls: Class to be modified with a package check.
+        class class_with_requires(cls):
         """
-        library_loaded = importlib.util.find_spec(package)
+        class class_with_requires(cls):
+            """ Adds a package check to the __init__ method of the parent class.
+            """
+            __doc__ = cls.__doc__
 
-        if (not library_loaded) & (not warn_only):
-            default_error = f"{cls.__name__} requires {package}"
+            def __init__(self, *args, **kwargs):
+                """ First checks if the specified package is installed then
+                proceeds with the parent's initialization.
 
-            raise ImportError(message or default_error)
+                Raises:
+                    ImportError: The required package is not found.
+                """
+                library_loaded = importlib.util.find_spec(package)
 
-        elif (not library_loaded):
-            default_warning = f"{package} not installed, proceeding anyways."
-            warnings.warn(message or default_warning, ImportWarning)
+                if (not library_loaded) & (not warn_only):
+                    default_error = f"{cls.__name__} requires {package}."
+                    raise ImportError(message or default_error)
 
-        return cls
+                elif (not library_loaded):
+                    default_warning = f"{package} not installed, proceeding."
+                    warnings.warn(message or default_warning, ImportWarning)
 
-    return check_package
+                super().__init__(*args, **kwargs)
+
+        return class_with_requires
+
+    return wrapper_factory
 
