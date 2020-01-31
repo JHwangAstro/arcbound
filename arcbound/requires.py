2ea2851f71a571c3edde10611074e538ed3f33e7
Jason Hwang <jhwang.astro@gmail.com>
2020-01-30 19:25:02 -0800

    Allow requires to return a function.

--- arcbound/requires.py
+++ arcbound/requires.py
@@ -4,7 +4,7 @@ or sends a warning otherwise.
 
 import functools
 import importlib.util
-from typing import Callable, TypeVar
+from typing import Callable, TypeVar, Union
 import warnings
 
 ClassType = TypeVar("ClassType")
@@ -14,7 +14,7 @@ def requires(
     message: str = None,
     warn_only: bool = False,
 ) -> Callable[[ClassType], ClassType]:
-    """ Returns the input class with an added check that the input package
+    """ Returns the input object with an added check that the input package
     is installed at instantiation.
 
     Args:
@@ -23,38 +23,59 @@ def requires(
         warn_only: Flag determining if import errors should be downgraded to
             a warning.
     """
-    def wrapper_factory(cls: ClassType) -> ClassType:
+    def wrapper_factory(
+        obj: Union[ClassType, Callable]
+    ) -> Union[ClassType, Callable]:
         """ Returns the input class with an added check that a specified package
         is installed at instantiation.
 
         Args:
-            cls: Class to be modified with a package check.
+            obj: Class or function to be modified with a package check.
         """
-        class class_with_requires(cls):
-            """ Adds a package check to the __init__ method of the parent class.
-            """
-            __doc__ = cls.__doc__
-
-            def __init__(self, *args, **kwargs):
-                """ First checks if the specified package is installed then
-                proceeds with the parent's initialization.
-
-                Raises:
-                    ImportError: The required package is not found.
+        if callable(obj):
+            @functools.wraps(obj)
+            def obj_with_requires(*args, **kwargs):
+                """ Adds a package check before executing the function.
                 """
                 library_loaded = importlib.util.find_spec(package)
 
                 if (not library_loaded) & (not warn_only):
-                    default_error = f"{cls.__name__} requires {package}."
+                    default_error = f"{obj.__name__} requires {package}."
                     raise ImportError(message or default_error)
 
                 elif (not library_loaded):
                     default_warning = f"{package} not installed, proceeding."
                     warnings.warn(message or default_warning, ImportWarning)
 
-                super().__init__(*args, **kwargs)
+                return obj(*args, **kwargs)
 
-        return class_with_requires
+        else:
+            class obj_with_requires(obj):
+                """ Adds a package check to the __init__ method of the parent
+                class.
+                """
+                __doc__ = obj.__doc__
+
+                def __init__(self, *args, **kwargs):
+                    """ First checks if the specified package is installed then
+                    proceeds with the parent's initialization.
+
+                    Raises:
+                        ImportError: The required package is not found.
+                    """
+                    library_loaded = importlib.util.find_spec(package)
+
+                    if (not library_loaded) & (not warn_only):
+                        default_error = f"{obj.__name__} requires {package}."
+                        raise ImportError(message or default_error)
+
+                    elif (not library_loaded):
+                        default_warning = f"{package} not installed, proceeding."
+                        warnings.warn(message or default_warning, ImportWarning)
+
+                    super().__init__(*args, **kwargs)
+
+        return obj_with_requires
 
     return wrapper_factory
 
